f3aaa12250ca4bc5e5f84403eb46e84e9cc5a115
wjwish <wjwish@126.com>
2014-12-17 11:16:04 +0800

    wjwish's programe3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 以下函数对用于缓存逆矩阵。在重复计算逆矩阵时，如果矩阵内容未做更改，先缓存逆矩阵，
+## 再次需要时则可从缓存查找，无需重复计算，这样可以节省系统计算时间。
 
-## Write a short comment describing this function
+
+## 这个函数是用于创建可缓存逆矩阵的特殊“矩阵”对象
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL                      
+        set <- function(y) {            #设置矩阵值
+                x <<- y                 #在其他环境中给x赋值
+                m <<- NULL              #在其他环境中，设置m初始值为NULL
+        }
+        get <- function() x             #获取矩阵值
+        setsolve <- function(solve) m <<- solve         #设置逆矩阵
+        getsolve <- function() m                        #获取逆矩阵
+        list(set = set, get = get,      #返回列表
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+## 这个函数用于计算上述makeCacheMatrix返回的特殊“矩阵”的逆矩阵
+## 如果已经计算逆矩阵，则将检索缓存中的逆矩阵，否则计算逆矩阵
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()               #将获得的x的逆矩阵返回给m
+        if(!is.null(m)) {               #判断缓存里m是否为空
+                message("getting cached data")          #如果缓存里有值，先返回一段文字
+                return(m)               #再返回缓存里的m值，即x的逆矩阵
+        }
+        data <- x$get()                 #如果缓存里没有值，把矩阵赋值给data
+        m <- solve(data, ...)           #计算逆矩阵，并赋值给m
+        x$setsolve(m)                   
+        m                               #返回逆矩阵
 }
